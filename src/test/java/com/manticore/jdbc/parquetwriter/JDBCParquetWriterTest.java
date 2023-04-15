f6550b0ad9d8010ffedd46464355b7c2abeeaa0a
Andreas Reichel <andreas@manticore-projects.com>
2023-04-15 22:54:24 +0700

    test: add test stub

---
+++ src/test/java/com/manticore/jdbc/parquetwriter/JDBCParquetWriterTest.java
@@ -0,0 +1,12 @@
+package com.manticore.jdbc.parquetwriter;
+
+import org.junit.jupiter.api.Test;
+
+class JDBCParquetWriterTest {
+
+  @Test
+  void write() {}
+
+  @Test
+  void getParquetSchemaFromResultSet() {}
+}
